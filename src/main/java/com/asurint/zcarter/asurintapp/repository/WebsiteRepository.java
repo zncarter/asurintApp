6f19db7ce2e39e314b0fc5bd417f91f01839b4cb
Zach Carter <zncarter@gmail.com>
2021-05-03 07:47:43 -0400

    Added findBySlug to WebsiteRepository to allow for loading website records by the slug.

--- src/main/java/com/asurint/zcarter/asurintapp/repository/WebsiteRepository.java
+++ src/main/java/com/asurint/zcarter/asurintapp/repository/WebsiteRepository.java
@@ -1,9 +1,13 @@
 package com.asurint.zcarter.asurintapp.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.asurint.zcarter.asurintapp.entity.Website;
 
+@RepositoryRestResource
 public interface WebsiteRepository extends CrudRepository<Website, Long> {
 
+	Website findBySlug(@Param("slug") String slug);
 }
